66b6c2001f045a8645b9c314116d3603f5030e87
songhsb <hssong404@gmail.com>
2023-11-02 23:50:42 +0900

    Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNF9Kzr9mXq1HWhVaSMI5kjBqGwyYf050",
+  authDomain: "nwitter-reloaded-fbe87.firebaseapp.com",
+  projectId: "nwitter-reloaded-fbe87",
+  storageBucket: "nwitter-reloaded-fbe87.appspot.com",
+  messagingSenderId: "90188249017",
+  appId: "1:90188249017:web:ef2b575a1ac40d31f6a868",
+};
+
+const app = initializeApp(firebaseConfig);
